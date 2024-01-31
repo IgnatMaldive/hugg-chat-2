999f9b2a2df03e439c39159121a545845512e105
Your Name <you@example.com>
2024-01-31 18:52:31 +0100

    rewind

--- api/index.py
+++ api/index.py
@@ -9,16 +9,6 @@ tavily = TavilyClient(api_key="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")
 
 CORS(app)
 
-def json_to_htmx(data):
-    # Convert JSON data to HTMX (HTML)
-    htmx_tags = ''
-    for result in data['results']:
-        htmx_tags += f"<div data-score='{result['score']}'>"
-        htmx_tags += f"<h1>{result['title']}</h1>"
-        htmx_tags += f"<p>{result['content']}</p>"
-        htmx_tags += f"<a href='{result['url']}'>Link</a>"
-        htmx_tags += '</div>'
-    return htmx_tags
 
 @app.route('/')
 def index():
@@ -32,18 +22,7 @@ def index():
 
 @app.route('/search', methods=['GET'])
 def search():
-    # Get the search query from the request
-    query = request.args.get('query', '')
-    if query:
-        # Perform the search using Tavily client
-        response = tavily.search(query=query)
-        # Convert the response to HTMX
-        htmx_output = json_to_htmx(response)
-        # Return the HTML response
-        return render_template_string(htmx_output)
-    else:
-        # If no query was provided
-        return "Please provide a search query."
+    print('holamanola')
 
 if __name__ == '__main__':
     app.run(debug=True)
