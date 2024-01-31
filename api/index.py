c27de40c1b1c5b522cf29d9b29c494e092b58804
Your Name <you@example.com>
2024-01-31 19:59:09 +0100

    manoa

--- api/index.py
+++ api/index.py
@@ -1,31 +1,29 @@
-from flask import Flask, request, render_template_string, jsonify
+from flask import Flask
+from tavily import TavilyClient
 from flask_cors import CORS
-from hugg_search import hugg_chat_query  # Importing the hugg_chat_query function
 
 app = Flask(__name__)
+
+# Initialize Tavily client with your API key
+tavily = TavilyClient(api_key="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")
+
 CORS(app)
+
+
 
 @app.route('/')
 def index():
+    # A simple form for entering search queries
     return '''
-        <form action="/chat" method="get">
+        <form action="/search" method="get">
             <input type="text" name="query" placeholder="Enter search query">
             <input type="submit" value="Search">
         </form>
     '''
 
-@app.route('/chat', methods=['GET'])
-def hugg_chat():
-    query = request.args.get('query', '')
-    try:
-        response = hugg_chat_query(query)
-        # Ensure response is a string and encode to bytes
-        if not isinstance(response, str):
-            response = str(response)
-        return response.encode('utf-8')
-    except Exception as e:
-        # Handle exceptions and return error message
-        return f"Error occurred: {str(e)}".encode('utf-8')
+@app.route('/search', methods=['GET'])
+def search():
+    return('holamanola')
 
 if __name__ == '__main__':
     app.run(debug=True)
