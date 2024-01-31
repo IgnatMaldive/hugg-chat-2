fbafee1e547792727fc3b73fd9edde909fdde6a4
Your Name <you@example.com>
2024-01-31 19:05:36 +0100

    clean

--- api/index.py
+++ api/index.py
@@ -1,24 +1,13 @@
-from flask import Flask, request, render_template_string
+from flask import Flask
 from tavily import TavilyClient
 from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Initialize Tavily client with your API key
-tavily = TavilyClient(api_key="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")
 
 CORS(app)
 
-def json_to_htmx(data):
-    # Convert JSON data to HTMX (HTML)
-    htmx_tags = ''
-    for result in data['results']:
-        htmx_tags += f"<div data-score='{result['score']}'>"
-        htmx_tags += f"<h1>{result['title']}</h1>"
-        htmx_tags += f"<p>{result['content']}</p>"
-        htmx_tags += f"<a href='{result['url']}'>Link</a>"
-        htmx_tags += '</div>'
-    return htmx_tags
+
 
 @app.route('/')
 def index():
@@ -32,7 +21,9 @@ def index():
 
 @app.route('/search', methods=['GET'])
 def search():
-    return('manola')
+    return('manolassdfas')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
