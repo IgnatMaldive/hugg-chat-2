aae619ae443e41abfe46fa6ea9d0cb81b4f01c56
Your Name <you@example.com>
2024-01-31 20:09:25 +0100

    solved

--- api/index.py
+++ api/index.py
@@ -4,6 +4,12 @@ from flask_cors import CORS
 from hugchat import hugchat
 from hugchat.login import Login
 
+# Log in to huggingface and grant authorization to huggingchat
+sign = Login("ignatiusmaldive@gmail.com", "hu&LO8f&e34")
+cookies = sign.login()
+
+
+
 app = Flask(__name__)
 
 
@@ -24,7 +30,26 @@ def index():
 
 @app.route('/search', methods=['GET'])
 def search():
-    return('holamanola')
+    try:
+        chatbot = hugchat.ChatBot(cookies=cookies.get_dict()) 
+        query_result = chatbot.query("my name is alfred")
+        if isinstance(query_result, str):
+            return query_result.encode('utf-8')
+        else:
+            # Handle non-string response appropriately
+            return str(query_result).encode('utf-8')
+    except GeneratorExit:
+        # Handle GeneratorExit exception
+        pass
+    except Exception as e:
+        # Handle other exceptions
+        return f"Error: {str(e)}".encode('utf-8')
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
